efa560915ef117735fbbed4c9652b58cbfdd61bf
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-04-27 14:39:44 +0100

    Added Player Controller

---
+++ models/player/player_model.go
@@ -0,0 +1,12 @@
+package models
+
+// Player :
+type Player struct {
+	ID             int
+	PlayerID       string
+	UserName       string
+	EmailAddress   string
+	DisplayName    string
+	AvatarURL      string
+	AvatarBlurHash string
+}
--- routers/routers.go
+++ routers/routers.go
@@ -1,11 +1,25 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	controllers "github.com/Blac-Panda/Stardome-API/controllers/players"
+	"github.com/gin-gonic/gin"
+)
 
 // Routers This  function defines the available routes
 // in Stardome API
 func Routers() *gin.Engine {
 	g := gin.Default()
+
+	api := g.Group("/api")
+	{
+		api.GET("/players", controllers.ListPlayers)
+		api.POST("/players", controllers.CreatePlayer)
+
+		api.GET("/players/:id", controllers.GetPlayer)
+		api.PUT("/players/:id", controllers.UpdatePlayer)
+		api.PATCH("/players/:id", controllers.ModifyPlayer)
+		api.DELETE("/players/:id", controllers.DeletePlayer)
+	}
 
 	return g
 }
