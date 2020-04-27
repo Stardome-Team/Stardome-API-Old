e2c3c97c53b3d2cb2ac41e0451c8d6512eb4311f
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-04-27 08:39:31 +0100

    Configured modd and added routers

---
+++ routers/routers.go
@@ -0,0 +1,11 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+// Routers This  function defines the available routes
+// in Stardome API
+func Routers() *gin.Engine {
+	g := gin.Default()
+
+	return g
+}
