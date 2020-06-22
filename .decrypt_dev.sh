02518629e0ba5108b1f9201f3ff2830bec4daf61
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-06-22 13:17:39 +0100

    updated file path in build scripts

--- .decrypt_dev.sh
+++ .decrypt_dev.sh
@@ -1,5 +1,5 @@
 
 # Decrypt .ENVIRONMENT.env file
 
-gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENV_SECRET_PASSPHRASE" \ --output .prod.env .prod.env.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENV_SECRET_PASSPHRASE" \ --output ./.prod.env ./.prod.env.gpg
 
