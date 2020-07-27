3c54be3ab9ad8786cfce309cd01eafbd9ac9c5e2
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-07-27 21:52:35 +0100

    first microservice split

--- .decrypt_dev.sh
+++
@@ -1,5 +0,0 @@
-
-# Decrypt .ENVIRONMENT.env file
-
-gpg --quiet --batch --yes --decrypt --passphrase="$DEV_ENV_SECRET_PASSPHRASE"  --output ./.dev.env ./.dev.env.gpg
-
--- services/service.go
+++
@@ -1,7 +0,0 @@
-package services
-
-import "github.com/Blac-Panda/Stardome-API/repositories"
-
-type service struct {
-	playerRepository repositories.PlayerRepository
-}
