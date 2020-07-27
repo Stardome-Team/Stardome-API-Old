4da0fddc208aa90d9689a16ad1f8ad4320255274
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-07-27 22:39:57 +0100

    Splitting projects into microservices

--- player-service/main.go
+++ player-service/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/spf13/viper"
 
@@ -16,15 +15,12 @@ func init() {
 	flag.Parse()
 
 	if *isProduction {
-		viper.SetConfigFile(".dev.env")
+		viper.SetConfigFile("../.prod.env")
 		viper.ReadInConfig()
 	} else {
-		viper.SetConfigFile(".prod.env")
+		viper.SetConfigFile("../.dev.env")
 		viper.ReadInConfig()
 	}
-
-	fmt.Printf("\n\n\n Secret -> %v \v\v\v", viper.GetString("JWT_SECRET_KEY"))
-
 }
 
 func main() {
