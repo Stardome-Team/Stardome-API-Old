896cbdd87a3c4cf071833a0714ba4ce719d9b9ec
Ogunye Nathaniel Oluwatobiloba <ogunye4nao@gmail.com>
2020-07-27 22:11:35 +0100

    code base clean up

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 
 	"github.com/spf13/viper"
 
@@ -22,9 +21,6 @@ func init() {
 		viper.SetConfigFile(".prod.env")
 		viper.ReadInConfig()
 	}
-
-	fmt.Printf("\n\n\n Secret -> %v \v\v\v", viper.GetString("JWT_SECRET_KEY"))
-
 }
 
 func main() {
